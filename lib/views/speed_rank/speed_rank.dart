61987bc3cadb7541b9501d4c5efdb3a000e6ff56
cc9527-1 <cc9527-1@users.noreply.github.com>
2026-06-07 11:47:49 +0800

    Fix: add core/core.dart import for coreController

--- lib/views/speed_rank/speed_rank.dart
+++ lib/views/speed_rank/speed_rank.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:fl_clash/common/common.dart';
+import 'package:fl_clash/core/core.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
 import 'package:fl_clash/providers/providers.dart';
